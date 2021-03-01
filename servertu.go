acde14a0c43dd53d1803f902ef8d3b9b43db6edb
emre <emrecanocak@windowslive.com>
2021-03-01 17:38:14 +0300

    DO NOT USE DEBUGGING PURPOSES

--- servertu.go
+++ servertu.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ecoshub/breakx"
 	"github.com/goburrow/serial"
 )
 
@@ -22,35 +23,48 @@ func (s *Server) ListenRTU(serialConfig *serial.Config, slaveID uint8) (err erro
 
 func (s *Server) acceptSerialRequests(port serial.Port, slaveID uint8) {
 	for {
+		breakx.Point()
 		buffer := make([]byte, 512)
 
 		bytesRead, err := port.Read(buffer)
+		breakx.Point()
 		if err != nil {
+			breakx.Point()
 			if err != io.EOF {
+				breakx.Point()
 				if s.onErrorhandler != nil {
+					breakx.Point()
 					(*s.onErrorhandler)(fmt.Errorf("[mbserver] serial read error %v", err))
 				}
 			}
 			return
 		}
 
+		breakx.Point()
 		if bytesRead != 0 {
 
 			// Set the length of the packet to the number of read bytes.
 			packet := buffer[:bytesRead]
 
+			breakx.Point()
 			frame, err := NewRTUFrame(packet)
 			if err != nil {
+				breakx.Point()
 				if s.onErrorhandler != nil {
+					breakx.Point()
 					(*s.onErrorhandler)(fmt.Errorf("[mbserver] serial read error %v", err))
 				}
 				return
 			}
+			breakx.Point()
 			if frame.GetAddress() == slaveID {
+				breakx.Point()
 				request := &Request{port, frame}
 				s.requestChan <- request
 			} else {
+				breakx.Point()
 				if s.onErrorhandler != nil {
+					breakx.Point()
 					(*s.onErrorhandler)(fmt.Errorf("[mbserver] serial read error %v", err))
 				}
 			}
